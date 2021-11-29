88217b29cbeb9a4ad8970cd49bd37b2a055cb963
KITTAPHORN JANPHENG <kittaphorn@KITTAPHORNs-MacBook-Pro.local>
2021-11-29 17:19:45 +0700

    delay with NOP

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -41,7 +41,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define SW_DEBOUNCE_TIME 130
+#define SW_DEBOUNCE_TIME 90
 #define IOT_SEND_INTERVAL 5
 #define CREDIT_HI_BYTE 0x06
 #define CREDIT_LO_BYTE 0x07
@@ -67,6 +67,7 @@ void do_500ms_tick(void);
 void do_400ms_tick(void);
 void do_300ms_tick(void);
 void do_200ms_tick(void);
+void do_2sec_tick(void);
 uint8_t read_button(void);
 void do_operation(void);
 void segment_display_int(int number);
@@ -93,6 +94,9 @@ void start_acceptors(void);
 
 void add_bank_note_credit(void);
 void add_coin_credit(void);
+
+void enable_exti(void);
+void disable_exti(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -102,6 +106,7 @@ volatile int tim2_300ms_counter = 0;
 volatile int tim2_200ms_counter = 0;
 volatile int tim2_400ms_counter = 0;
 volatile int tim2_500ms_counter = 0;
+volatile int tim2_2sec_counter = 0;
 volatile int iot_round_counter = 0;
 volatile bool is_operation_running = false;
 volatile uint16_t credit = 0;   // credit store
@@ -145,45 +150,48 @@ volatile const uint8_t default_credit_duration = 10;
 
 volatile uint8_t last_note_money = 0;
 volatile uint8_t last_coin_money = 0;
+
+volatile bool exti_disable = false;
 ////////////////////////////////////
 
 
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_RTC_Init();
-  MX_SPI2_Init();
-  MX_TIM2_Init();
-  MX_USART1_UART_Init();
-  MX_USART3_UART_Init();
-  /* USER CODE BEGIN 2 */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_RTC_Init();
+	MX_SPI2_Init();
+	MX_TIM2_Init();
+	MX_USART1_UART_Init();
+	MX_USART3_UART_Init();
+	/* USER CODE BEGIN 2 */
+	disable_exti();
 	reset_all_pins();
 	read_settings_from_eeprom();
 	init_display();
@@ -191,15 +199,17 @@ int main(void)
 	//	HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_2);
 	reset_all_pins();
 	HAL_TIM_Base_Start_IT(&htim2);
-  /* USER CODE END 2 */
+	HAL_Delay(2000);
+	enable_exti();
+	/* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
 	while (1)
 	{
-    /* USER CODE END WHILE */
+		/* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+		/* USER CODE BEGIN 3 */
 		uint8_t btn_read = read_button();
 		if(btn_read == 6){ // credit reset is press
 			reset_all_state();
@@ -228,58 +238,74 @@ int main(void)
 			reset_all_pins();
 		}
 		HAL_Delay(120);
-//		start_acceptors();
+		//		start_acceptors();
 
 	}
-  /* USER CODE END 3 */
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+	RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+			|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_RTC;
-  PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_RTC;
+	PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
+	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+	{
+		Error_Handler();
+	}
 }
 
 /* USER CODE BEGIN 4 */
+void disable_exti(){
+	exti_disable = true;
+	HAL_NVIC_DisableIRQ(EXTI4_IRQn);
+	HAL_NVIC_DisableIRQ(EXTI9_5_IRQn);
+	HAL_UART_Transmit(&huart1, (uint8_t*)"\n##### EXTI DISABLED!! ######\r\n", 31,HAL_MAX_DELAY);
+}
+void enable_exti(){
+	exti_disable = false;
+	__HAL_GPIO_EXTI_CLEAR_IT(COIN_IT_Pin);
+	__HAL_GPIO_EXTI_CLEAR_IT(BANK_IT_Pin);
+	HAL_NVIC_ClearPendingIRQ(EXTI4_IRQn);
+	HAL_NVIC_ClearPendingIRQ(EXTI9_5_IRQn);
+	HAL_NVIC_EnableIRQ(EXTI4_IRQn);
+	HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
+	HAL_UART_Transmit(&huart1, (uint8_t*)"\n##### EXTI ENABLE ######\r\n", 27,HAL_MAX_DELAY);
+}
 void send_iot_status(uint8_t money_event){
 	HAL_UART_Transmit(&huart1, (uint8_t*)">>>>>>>>> sending iot package\r\n", 31,HAL_MAX_DELAY);
 	uint8_t iot_serial_package[8];
@@ -473,9 +499,9 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 		tim2_400ms_counter += 1;
 		tim2_200ms_counter += 1;
 		if(tim2_counter % 10 == 0){ /// 100 ms tick
-//			char tmp_msg[35];
-//			sprintf(tmp_msg,"pressed button : %d \r\n",pressed_button);
-//			HAL_UART_Transmit(&huart1, (uint8_t *)tmp_msg, strlen(tmp_msg), HAL_MAX_DELAY);
+			//			char tmp_msg[35];
+			//			sprintf(tmp_msg,"pressed button : %d \r\n",pressed_button);
+			//			HAL_UART_Transmit(&huart1, (uint8_t *)tmp_msg, strlen(tmp_msg), HAL_MAX_DELAY);
 			if(setting_mode != 0){
 				is_standby = false;
 				display_menu(setting_mode);
@@ -580,6 +606,7 @@ void decrease_credit(){
 	}
 }
 void do_1sec_tick(){
+
 	HAL_UART_Transmit(&huart1, (uint8_t*)"1sec tick\r\n", 11,HAL_MAX_DELAY);
 	char tmp_msg[35];
 	sprintf(tmp_msg,"#### current credit: %d, mode: %d\r\n",credit,pressed_button);
@@ -599,7 +626,9 @@ void do_1sec_tick(){
 			last_note_money = 0;
 		}
 	}
+
 }
+
 void do_500ms_tick(){
 
 }
@@ -609,9 +638,9 @@ void do_200ms_tick(){
 	}else{
 		front_button_reset_credit_counter = 0;
 	}
-//	char tmp_msg[35];
-//	sprintf(tmp_msg,"===== front button counter: %d\r\n",front_button_reset_credit_counter);
-//	HAL_UART_Transmit(&huart1, (uint8_t *)tmp_msg, strlen(tmp_msg), HAL_MAX_DELAY);
+	//	char tmp_msg[35];
+	//	sprintf(tmp_msg,"===== front button counter: %d\r\n",front_button_reset_credit_counter);
+	//	HAL_UART_Transmit(&huart1, (uint8_t *)tmp_msg, strlen(tmp_msg), HAL_MAX_DELAY);
 	if(is_standby){
 		segment_display_standby();
 	}
@@ -650,14 +679,18 @@ void reset_all_pins(){
 	HAL_GPIO_WritePin(OUT_5_GPIO_Port, OUT_5_Pin, GPIO_PIN_RESET); // pin b9 --> out 5
 	HAL_GPIO_WritePin(OUT_6_GPIO_Port, OUT_6_Pin, GPIO_PIN_RESET); // pin b12 --> out 6
 	current_out_port = 0;
+	enable_exti();
 }
 
 void set_output_to(uint8_t pin){
 	if(current_out_port == pin){
 		return;
 	}
-
 	reset_all_pins();
+	disable_exti();  //// to prevent noise from relay
+	char tmp_msg[50];
+	sprintf(tmp_msg, ">>>>>>>> SET OUTPUT => %d \r\n", (int)pin);
+	HAL_UART_Transmit(&huart1, (uint8_t*)tmp_msg, strlen(tmp_msg), HAL_MAX_DELAY);
 	switch(pin){
 	case 1:
 		HAL_GPIO_WritePin(OUT_1_GPIO_Port, OUT_1_Pin, GPIO_PIN_SET); // pin b0 --> out 1
@@ -690,8 +723,13 @@ void set_output_to(uint8_t pin){
 		current_out_port = 0;
 		break;
 	}
-//	HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_1);
-//	HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_2);
+	int i=0;
+	for(i=0; i<2000000;i++){
+		asm("NOP");
+	}
+	enable_exti();
+	//	HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_1);
+	//	HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_2);
 }
 
 void do_operation(){
@@ -942,34 +980,34 @@ void HAL_GPIO_EXTI_Callback( uint16_t GPIO_Pin)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
+	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1)
 	{
 	}
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
+	/* USER CODE BEGIN 6 */
 	/* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+	/* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
 
